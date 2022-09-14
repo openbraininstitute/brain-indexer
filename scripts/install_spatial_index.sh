37fd27a27c7d4ad45913388eb875a506417e1131
Antonio Bellotta <antonio.bellotta@epfl.ch>
2022-09-14 09:10:09 +0000

    Update install script
    Updated the install script to make it work with the latest version of SpatialIndex.

--- scripts/install_spatial_index.sh
+++ scripts/install_spatial_index.sh
@@ -16,6 +16,6 @@ else
     python -m venv "$1"
 fi
 
-module load unstable cmake gcc boost/1.78.0
+module load unstable cmake gcc boost hpe-mpi
 . "$1"/bin/activate
 pip install git+ssh://git@bbpgitlab.epfl.ch/hpc/SpatialIndex.git
