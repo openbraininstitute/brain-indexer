8ec099f518bc43b42be7225c94e0c645e3ce359a
Antonio Bellotta <antonio.bellotta@epfl.ch>
2023-02-02 09:14:32 +0000

    Added consistency check for SONATA in integration tests
    Added a new test for consistency with SONATA.
    
    The test was added to the integration.
    
    Closes SPIND-246

--- .ci/_run_integration.sh
+++ .ci/_run_integration.sh
@@ -10,6 +10,7 @@ rm -rf tmp-* || true
 python3 .ci/test_validation_FLAT.py
 srun -n5 python3 .ci/test_validation_FLAT.py --run-multi-index
 python3 .ci/test_bluepy.py
+python3 .ci/test_sonata_sanity.py
 
 set +x
 echo "[`date`] Integration Tests Finished"
